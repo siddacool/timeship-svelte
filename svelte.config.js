4b44e48a9fc081f282ac6bfef24c23835777acec
Siddhesh Mangela <sidthecool007@gmail.com>
2022-08-04 11:49:43 +0530

    svelte SPA added

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,8 @@ const config = {
 
   kit: {
     adapter: adapter({
-      fallback: null,
+      fallback: 'index.html',
     }),
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true,
-    },
   },
 };
 
