63be64da825ae9539e6736738828e0942d11ea06
Siddhesh Mangela <sidthecool007@gmail.com>
2022-08-04 14:46:07 +0530

    sveltekit service workers

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,22 +9,3 @@ declare namespace App {
   // interface Session {}
   // interface Stuff {}
 }
-
-declare module 'virtual:pwa-register/svelte' {
-  // @ts-expect-error ignore when svelte is not installed
-  import type { Writable } from 'svelte/store';
-
-  export interface RegisterSWOptions {
-    immediate?: boolean;
-    onNeedRefresh?: () => void;
-    onOfflineReady?: () => void;
-    onRegistered?: (registration: ServiceWorkerRegistration | undefined) => void;
-    onRegisterError?: (error: any) => void;
-  }
-
-  export function useRegisterSW(options?: RegisterSWOptions): {
-    needRefresh: Writable<boolean>;
-    offlineReady: Writable<boolean>;
-    updateServiceWorker: (reloadPage?: boolean) => Promise<void>;
-  };
-}
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import { pwaConfiguration } from './pwa-configuration.js';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [VitePWA(pwaConfiguration), sveltekit()],
+  plugins: [sveltekit()],
 };
 
 export default config;
